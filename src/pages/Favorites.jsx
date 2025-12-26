e28c24b5d4afed9045bba9f1f6bc1df1283171e8
Carloo1234 <carlokarim9129@gmail.com>
2025-12-26 18:23:00 +0200

    Initial commit: Movie app with API handling.

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,32 @@
+import "../css/Favorites.css"
+import { useMovieContext } from "../contexts/MovieContext";
+import Card from "../components/Card";
+
+function Favorites() {
+  const {favorites} = useMovieContext();
+
+  if (favorites) {
+    return (
+      <div>
+        <h2 className="favorites">Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map(
+            (movie) => (
+                <Card movie={movie} key={movie.id} />
+              )
+          )}
+        </div>
+      </div>
+    );
+  }
+  
+  
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies yet.</h2>
+      <p>Start adding movies to your favorites and they will apear here!</p>
+    </div>
+  );
+}
+
+export default Favorites;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,82 @@
+import Card from "../components/Card";
+import "../css/Home.css";
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+      try {
+        const popularMovies = await getPopularMovies();
+        setMovies(popularMovies);
+      } catch (error) {
+        console.log(error);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadPopularMovies();
+  }, []);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (!searchQuery.trim()) return;
+    if (loading) return; // Prevent searching if already loading
+
+    setLoading(true);
+    try {
+      const serarchResults = await searchMovies(searchQuery);
+      setMovies(serarchResults);
+      setError(null);
+    } catch (error) {
+      console.log(error);
+      setError("Failed to search movies...");
+    } finally {
+      setLoading(false);
+    }
+
+    setSearchQuery("");
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          name="search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+      {error && <div className="error-message">{error}</div>}
+
+      {loading ? (
+        <div className="loading-container">
+          <div className="spinner"></div>
+          <p>Fetching movies...</p>
+        </div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map((movie) => (
+            <Card movie={movie} key={movie.id} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
